a4ff244ef0b6580823347c7307710695f4af2799
Lim Zhen Ming <limzming@gmail.com>
2015-03-08 14:10:49 +0800

    Change Object to introduce addition of other type of tasks

--- src/com/done/Done.java
+++ src/com/done/Done.java
@@ -1,12 +1,32 @@
 package com.done;
 
 public class Done {
+	private int id;
 	private String title;
+	private String date;
+	private String startTime;
+	private String endTime;
 	
-	public Done(String title){
+	// Floating Task constructor
+	public Done(int id, String title){
+		this.id = id;
 		this.title = title;
 	}
-
+	
+	// Timed Task constructor
+	public Done(int id, String title, String startTime, String endTime){
+		this.id = id;
+		this.title = title;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	// Deadline Task constructor
+	public Done(int id, String title, String date){
+		this.id = id;
+		this.title = title;
+		this.date = date;
+	}
 	/**
 	 * @return the title
 	 */
