21d4376ca473071dcd0287e6711a3f0b4f7de222
frmlenx <frm10802@hotmail.com>
2015-03-10 16:17:44 +0800

    Added support to all relevant command words
    Added parser support to include all relevant command words and the
    parser could now return all command types

--- src/com/done/parser/CommandParser.java
+++ src/com/done/parser/CommandParser.java
@@ -3,36 +3,52 @@ package com.done.parser;
 import java.util.ArrayList;
 
 public class CommandParser {
-	
-	/* TODO: These commands are for skeletal purpose only
-	** DISPLAY and CLEAR command especially will be replaced/removed
-	*/
-	public enum CommandType{
-		ADD, DELETE, CLEAR, DISPLAY, EXIT, INVALID;
+
+	/*
+	 * TODO: These commands are for skeletal purpose only* DISPLAY and CLEAR
+	 * command especially will be replaced/removed
+	 */
+	public enum CommandType {
+		ADD, DELETE, CLEAR, DISPLAY, EDIT, UNDO, REORDER, MOVE, MARK, REMIND, RECUR, EXIT, INVALID;
 	}
-	
-	public CommandType getCommandType(String userCommand){
-		
+
+	public CommandType getCommandType(String userCommand) {
+
 		String command = CommandUtils.getFirstWord(userCommand);
-		
-		if(command.equalsIgnoreCase("add")){
+
+		if (command.equalsIgnoreCase("add")) {
 			return CommandType.ADD;
-		}else if(command.equalsIgnoreCase("delete")){
+		} else if (command.equalsIgnoreCase("delete")) {
 			return CommandType.DELETE;
-		}else if(command.equalsIgnoreCase("display")){
+		} else if (command.equalsIgnoreCase("display")) {
 			return CommandType.DISPLAY;
-		}else if(command.equalsIgnoreCase("clear")){
+		} else if (command.equalsIgnoreCase("clear")) {
 			return CommandType.CLEAR;
-		}else if(command.equalsIgnoreCase("exit")){
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.EDIT;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.UNDO;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.REORDER;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.MOVE;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.MARK;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.REMIND;
+		} else if (command.equalsIgnoreCase("exit")) {
+			return CommandType.RECUR;
+		} else if (command.equalsIgnoreCase("exit")) {
 			return CommandType.EXIT;
-		}else{
+		} else {
 			return CommandType.INVALID;
 		}
 	}
-	
-	public ArrayList<String> getCommandContent (String userCommand){
+
+	public ArrayList<String> getCommandContent(String userCommand) {
 		String currentContent = CommandUtils.removeFirstWord(userCommand);
-		ArrayList<String> commandContent = CommandUtils.processContent(currentContent);
+		ArrayList<String> commandContent = CommandUtils
+				.processContent(currentContent);
 		return commandContent;
 	}
 }
