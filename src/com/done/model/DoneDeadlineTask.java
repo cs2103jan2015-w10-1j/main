4c167f36316253753fd59f823a57319935addeed
Lim Zhen Ming <limzming@gmail.com>
2015-03-16 19:31:29 +0800

    Minor change to visibility

--- src/com/done/model/DoneDeadlineTask.java
+++ src/com/done/model/DoneDeadlineTask.java
@@ -2,7 +2,7 @@ package com.done.model;
 
 public class DoneDeadlineTask extends Done {
 	// Time format to use UNIX epoch style
-	long endTime;
+	private long endTime;
 	
 	public DoneDeadlineTask(String title, long endTime){
 		super(title);
