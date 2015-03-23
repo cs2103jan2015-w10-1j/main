0d99e402ba174ed9ffe606709c9a52fc3299672a
ascii80 <p.maetasatidsuk@gmail.com>
2015-03-23 17:47:59 +0800

    Add getTasks() skeleton to LogicFacade

--- src/com/done/logic/LogicFacade.java
+++ src/com/done/logic/LogicFacade.java
@@ -1,11 +1,20 @@
 package com.done.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.done.result.ExecutionResult;
 import com.done.parser.CommandParser.CommandType;
+import com.done.model.Done;
 
 public class LogicFacade {
 	public ExecutionResult getExecutionResult(String userCommand){
 		ExecutionResult tempExecutionResult = new ExecutionResult(CommandType.ADD, true, "Task 1 added.");
 		return tempExecutionResult;	
+	}
+	
+	public List<Done> getTasks(){
+		List<Done> tempList = new ArrayList<Done>();
+		return tempList;
 	}
 }
