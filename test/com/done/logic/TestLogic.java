8ba58b5ba53168a572628ed44b7e631d8b4f9821
zaclizitong <zaclizitong@gmail.com>
2015-03-25 02:53:49 +0800

    Unit testing for Logic

---
+++ test/com/done/logic/TestLogic.java
@@ -0,0 +1,52 @@
+package com.done.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.done.logic;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.done.model.Done;
+import com.done.model.DoneFloatingTask;
+import com.done.storage.DoneStorage;
+import com.done.storage.InMemStorage;
+ 
+import com.done.parser.CommandParser;
+import com.done.parser.CommandParser.CommandType;
+import com.done.parser.ParserUtils;
+
+public class TestLogic{
+	
+	private static final int ZERO = 0;
+	
+	Logic testLogic = new Logic();
+	
+	@Test
+	public void testAddTask(){
+		
+		String testString = "hahaha";
+		testLogic.addTask(testString);
+		assertEquals(testLogic.getTasksForUI().get(ZERO), testString);
+	}
+	
+	@Test
+	public void testDeleteTask(){
+		
+		testLogic.deleteTask(ZERO);
+		assertEquals(testLogic.getTasksForUI().length, ZERO);
+	}
+	
+	@Test
+	public void testExecuteCommand(){
+		String testString1 = "add Batman";
+		String testString2 = "add Ironman is the best";
+		String testString3 = "delete Batman"
+		testLogic.executeCommand(testString1);
+		testLogic.executeCommand(testString2);
+		testLogic.executeCommand(testString3);
+		assertEquals(estLogic.getTasksForUI().get(ZERO), testString2);
+		
+	}
+}
