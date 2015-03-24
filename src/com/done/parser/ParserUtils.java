a6f9819ae9b0adadf232b77d82f7fa24a1d9e4f0
frmlenx <frm10802@hotmail.com>
2015-03-24 17:21:57 +0800

    Added validation method for commandContents
    For some commands(such as add and delete), it is important to ensure
    that the content of the command line is as designated. Therefore, a
    validation method is implemented to test out if the contents are
    compliant to the standard before making the contents into a Command
    object. Right now it just support “add”,  “delete” and “mark”. Will
    extend in the future.

--- src/com/done/parser/ParserUtils.java
+++ src/com/done/parser/ParserUtils.java
@@ -2,31 +2,30 @@ package com.done.parser;
 
 import java.util.ArrayList;
 
-import com.done.parser.Command.CommandType;
 
 public class ParserUtils {
-	
-	public static String removeFirstWord(String userCommand) {
-        return userCommand.replace(getFirstWord(userCommand), "").trim();
-    }
-	
-	public static String getFirstWord(String userCommand) {
-        return userCommand.trim().split("\\s+")[0];
-    }
 
-	//At current Stage, only slice the content into parts separated by spaces
-	//Might modify in later versions
-	public static ArrayList<String> processContent(String content){
+	public static String removeFirstWord(String userCommand) {
+		return userCommand.replace(getFirstWord(userCommand), "").trim();
+	}
+
+	public static String getFirstWord(String userCommand) {
+		return userCommand.trim().split("\\s+")[0];
+	}
+
+	// At current Stage, only slice the content into parts separated by spaces
+	// Might modify in later versions
+	public static ArrayList<String> processContent(String content) {
 		ArrayList<String> processedContent = new ArrayList<String>();
-		String[]contentPieces = content.split("\\s+");
-		for(String pieceOfContent:contentPieces){
+		String[] contentPieces = content.split("\\s+");
+		for (String pieceOfContent : contentPieces) {
 			processedContent.add(pieceOfContent);
 		}
 		return processedContent;
 	}
-	
-	public static Command makeCommand(String commandWord, String commandContent){
-		//Command command;
+
+	public static Command makeCommand(String commandWord, String commandContent) {
+		// Command command;
 		if (commandWord.equalsIgnoreCase("add")) {
 			return new CommandAdd(commandContent);
 		} else if (commandWord.equalsIgnoreCase("delete")) {
@@ -35,8 +34,29 @@ public class ParserUtils {
 			return new CommandClear();
 		} else if (commandWord.equalsIgnoreCase("search")) {
 			return new CommandSearch(commandContent);
-		} else{
+		} else {
 			return new CommandInvalid();
+		}
+	}
+
+	public static boolean isContentValid(String commandWord,
+			String commandContent) {
+		if (commandWord.equalsIgnoreCase("add")) {
+			return true;
+		} else if (commandWord.equalsIgnoreCase("delete")
+				|| commandWord.equalsIgnoreCase("mark")) {
+			return isPositiveNonZeroInt(commandContent);
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isPositiveNonZeroInt(String content) {
+		try {
+			int i = Integer.parseInt(content);
+			return (i > 0 ? true : false);
+		} catch (NumberFormatException nfe) {
+			return false;
 		}
 	}
 }
