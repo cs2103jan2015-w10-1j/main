b9a4e807b9c45f81d4e5c89c605a5b117ca54e0f
ascii80 <p.maetasatidsuk@gmail.com>
2015-03-30 16:12:29 +0800

    Add addUI and notifyUIs in LogicFacade (note that notifyUIs has to be modified to access Logic)

--- src/com/done/logic/LogicFacade.java
+++ src/com/done/logic/LogicFacade.java
@@ -10,10 +10,12 @@ import com.done.command.Command;
 import com.done.command.Command.CommandType;
 import com.done.model.Done;
 import com.done.logic.Logic;
+import com.done.observer.Observer;
 
 public class LogicFacade {
 	
 	private Logic logic;
+	private List<Observer> observerList = new ArrayList<Observer>();
 	
 	public ExecutionResult getExecutionResult(String userCommand){
 
@@ -51,5 +53,16 @@ public class LogicFacade {
 		JSONStorage jsonStorage = JSONStorage.getInstance();
 		String jsonName = jsonStorage.getJsonNameFromPref();
 		return jsonName;
+	}
+	
+	public void addUI(Observer o){
+		observerList.add(o);
+	}
+	
+	public void notifyUIs(){
+		int taskId = 1; //this has to be changed to the task id needed to be reminded - put to avoid compilation error
+		for(Observer o: observerList){
+			o.updateReminder(taskId);
+		}
 	}
 }
