ddb97f9e55958ee5c851dc658420e13244c9e58d
frmlenx <frm10802@hotmail.com>
2015-04-07 18:12:37 +0800

    implemented Command ClearDone to clear all the done tasks, added support in parser and inMeMstorage

---
+++ src/com/done/command/CommandClearDone.java
@@ -0,0 +1,28 @@
+package com.done.command;
+
+import java.util.logging.Level;
+
+import com.done.storage.InMemStorage;
+
+public class CommandClearDone extends Command {
+	
+	public CommandClearDone(){
+		super(CommandType.CLEARDONE,false);
+		commandLogger.log(Level.INFO, "ClearDone Command Created");
+	}
+
+	@Override
+	public void execute() throws Exception {
+		commandLogger.log(Level.INFO, "Cleardone Command Execution called");
+		InMemStorage.getInstance().clearDoneTasks();
+		commandLogger.log(Level.INFO, "cleardone Command Execution succeeded");
+	}
+
+	//undo not required in CLEARDONE
+	@Override
+	public void undo() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
